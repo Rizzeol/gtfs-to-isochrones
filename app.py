edd1cabfeab1a3c379f5e85b087ad3f975e7101b
Basile Bruneau <basilebruneau@gmail.com>
2018-11-25 05:10:11 +0100

    Use json and POST to exchange with the second backend

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 import requests
 from flask import Flask, request, jsonify
 from gtfs_graph.geo import get_isochrone, stops
+from flask_cors import CORS
+import json
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def index():
@@ -14,20 +16,20 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def search():
-    pois = request.form['poi']
-    max_price = request.form['max_price']
+    pois = request.json['poi']
+    max_price = request.json['max_price']
     isochrone = None
     all_paths = list()
     for petit in pois:
         gs, paths = get_isochrone(petit['lat'], petit['lon'], petit['duration'])
         isochrone = gs if isochrone is None else isochrone.union(gs)
         all_paths.append(paths)
-    r = requests.get('localhost:3000/annonces', params={
+    r = requests.post('http://localhost:3000/annonces', headers={'Content-Type': 'application/json'}, data=json.dumps({
         'isochrone': isochrone.__geo_interface__,
         'max_price': max_price,
-    })
+    }))
     matching_properties = r.json()
-    matching_properties[paths] = all_paths
+    matching_properties['paths'] = all_paths
     matching_properties['stops'] = get_stop_locations_from_paths(all_paths)
     return jsonify(matching_properties)
 
